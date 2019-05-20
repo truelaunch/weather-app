caf46157cbeb4bf4f867229228a70f6df519b490
Joanna Bennett <joanna@mandrill.com>
2019-05-20 11:51:58 -0400

    Add getWeather method to make API call to openweathermap.org

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import React from 'react';
 import Titles from './components/titles';
 import Form from './components/form';
 import Weather from './components/weather';
+
+// ?? How is this file running locally (browser interpreting JSX, etc) without running npm run build?
+
 // ?? Class based component. Likely to have state??
 class App extends React.Component {
+  getWeather = async (e) => {
+    // prevent default browser behavior of full page refresh
+    e.preventDefault();
+    const Api_Key = "fdb1ebbb01af7dc02ca2faf5f980f3a6";
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${Api_Key}`);
+    const response = await api_call.json();
+    console.log(response);
+  }
+
   render() {
     return (
       <div>
